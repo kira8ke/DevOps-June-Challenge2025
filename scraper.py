9be0d40f910f91db7eeeac208e03f028d7fc1ef8
kira8ke <wanyagagloria43@gmail.com>
2025-06-24 22:22:03 +0300

    Test 2 on functionality

---
+++ scraper.py
@@ -0,0 +1,11 @@
+#scrapper py
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://example.com"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+title = soup.title.string
+print(f"Website Title: {title}")
